177bdd9eb0c37256ffb51cc8cd70c85eb819c632
Leopoldo Pla <leopoldo.pla@treelogic.com>
2026-08-11 23:25:32 +0200

    fix: stub esp_read_mac for current firmware

--- src/esp_mac.h
+++ src/esp_mac.h
@@ -1,10 +1,29 @@
 #pragma once
+
+#include "esp_err.h"
+
 #include <cstdint>
 #include <cstring>
 
+typedef enum {
+  ESP_MAC_WIFI_STA,
+  ESP_MAC_WIFI_SOFTAP,
+  ESP_MAC_BT,
+  ESP_MAC_ETH,
+  ESP_MAC_IEEE802154,
+  ESP_MAC_BASE,
+  ESP_MAC_EFUSE_FACTORY,
+  ESP_MAC_EFUSE_CUSTOM,
+} esp_mac_type_t;
+
+static const uint8_t SIMULATOR_MAC[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x01};
+
 // Simulator stub: return a fixed fake MAC address
 static inline int esp_efuse_mac_get_default(uint8_t mac[6]) {
-  static const uint8_t fakeMac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x01};
-  memcpy(mac, fakeMac, 6);
-  return 0;
+  memcpy(mac, SIMULATOR_MAC, sizeof(SIMULATOR_MAC));
+  return ESP_OK;
+}
+
+static inline esp_err_t esp_read_mac(uint8_t mac[6], esp_mac_type_t) {
+  return esp_efuse_mac_get_default(mac);
 }
