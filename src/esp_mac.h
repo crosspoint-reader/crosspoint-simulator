bd384d12ba0b4facb1b172f7e4a6e964d32f6608
Leopoldo Pla <leopoldo.pla@treelogic.com>
2026-08-12 12:49:19 +0200

    fix: match ESP-IDF MAC error contract

--- src/esp_mac.h
+++ src/esp_mac.h
@@ -14,16 +14,19 @@ typedef enum {
   ESP_MAC_BASE,
   ESP_MAC_EFUSE_FACTORY,
   ESP_MAC_EFUSE_CUSTOM,
+  ESP_MAC_EFUSE_EXT,
 } esp_mac_type_t;
 
 static const uint8_t SIMULATOR_MAC[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x01};
 
 // Simulator stub: return a fixed fake MAC address
-static inline int esp_efuse_mac_get_default(uint8_t mac[6]) {
+static inline esp_err_t esp_efuse_mac_get_default(uint8_t mac[6]) {
+  if (!mac) return ESP_ERR_INVALID_ARG;
   memcpy(mac, SIMULATOR_MAC, sizeof(SIMULATOR_MAC));
   return ESP_OK;
 }
 
-static inline esp_err_t esp_read_mac(uint8_t mac[6], esp_mac_type_t) {
+static inline esp_err_t esp_read_mac(uint8_t mac[6], esp_mac_type_t type) {
+  if (type != ESP_MAC_WIFI_STA) return ESP_ERR_INVALID_ARG;
   return esp_efuse_mac_get_default(mac);
 }
